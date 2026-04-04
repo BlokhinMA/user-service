0216ba2f626948895c57633598d7ff98ab6bf4f6
Mikhail Blokhin <b.spillbert2016@gmail.com>
2026-04-04 10:49:13 +0400

    Swagger, HATEOAS

--- src/main/java/ru/mikhailblokhin/userservice/controllers/UserController.java
+++ src/main/java/ru/mikhailblokhin/userservice/controllers/UserController.java
@@ -18,6 +18,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private static final String CREATE = "create";
+    private static final String READ = "read";
+    private static final String UPDATE = "update";
+    private static final String DELETE = "delete";
+    private static final String READ_ALL = "read_all";
+    
     private final UserService service;
 
     public UserController(UserServiceImpl service) {
@@ -29,10 +35,10 @@ public class UserController {
         UserResponseDto userResponseDto = service.create(dto);
 
         userResponseDto.add(linkTo(methodOn(UserController.class).create(dto)).withSelfRel());
-        userResponseDto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel("read"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).update(dto)).withRel("update"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel("delete"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel("read-all"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel(READ));
+        userResponseDto.add(linkTo(methodOn(UserController.class).update(dto)).withRel(UPDATE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel(DELETE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel(READ_ALL));
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -43,11 +49,11 @@ public class UserController {
     public ResponseEntity<UserResponseDto> read(@PathVariable("id") Long id) {
         UserResponseDto userResponseDto = service.read(id);
 
-        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel("create"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel(CREATE));
         userResponseDto.add(linkTo(methodOn(UserController.class).read(id)).withSelfRel());
-        userResponseDto.add(linkTo(methodOn(UserController.class).update(new UserRequestDto())).withRel("update"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).delete(id)).withRel("delete"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel("read-all"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).update(new UserRequestDto())).withRel(UPDATE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).delete(id)).withRel(DELETE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel(READ_ALL));
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -58,11 +64,11 @@ public class UserController {
     public ResponseEntity<UserResponseDto> update(@RequestBody UserRequestDto dto) {
         UserResponseDto userResponseDto = service.update(dto);
 
-        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel("create"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel("read"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel(CREATE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel(READ));
         userResponseDto.add(linkTo(methodOn(UserController.class).update(dto)).withSelfRel());
-        userResponseDto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel("delete"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel("read-all"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel(DELETE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel(READ_ALL));
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -73,8 +79,8 @@ public class UserController {
     public ResponseEntity<UserResponseDto> delete(@PathVariable("id") Long id) {
         UserResponseDto userResponseDto = service.delete(id);
 
-        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel("create"));
-        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel("read-all"));
+        userResponseDto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel(CREATE));
+        userResponseDto.add(linkTo(methodOn(UserController.class).readAll()).withRel(READ_ALL));
 
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -86,15 +92,15 @@ public class UserController {
         List<UserResponseDto> dtos = service.readAll();
 
         for (UserResponseDto dto : dtos) {
-            dto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel("create"));
-            dto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel("read"));
-            dto.add(linkTo(methodOn(UserController.class).update(new UserRequestDto())).withRel("update"));
-            dto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel("delete"));
+            dto.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel(CREATE));
+            dto.add(linkTo(methodOn(UserController.class).read(dto.getId())).withRel(READ));
+            dto.add(linkTo(methodOn(UserController.class).update(new UserRequestDto())).withRel(UPDATE));
+            dto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel(DELETE));
             dto.add(linkTo(methodOn(UserController.class).readAll()).withSelfRel());
         }
 
         CollectionModel<UserResponseDto> collectionModel = CollectionModel.of(dtos);
-        collectionModel.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel("create"));
+        collectionModel.add(linkTo(methodOn(UserController.class).create(new UserRequestDto())).withRel(CREATE));
         collectionModel.add(linkTo(methodOn(UserController.class).readAll()).withSelfRel());
 
         return ResponseEntity
